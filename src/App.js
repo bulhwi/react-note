4b2cdc10708fcb31f12ec1f42da64e72655f82d6
bulhwi <mm225012@gmail.com>
2021-03-08 00:07:25 +0900

    react 정리중

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Say from "./Say";
+// import Counter from "./Counter";
+// import MyComponent from "./MyComponent";
+
+
+const App = () => {
+  return <Say />
+};
+
+export default App;
