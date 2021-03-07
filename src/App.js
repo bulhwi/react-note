06e3b58e8f5f80ca7e84160c6307bb652d176689
bulhwi <mm225012@gmail.com>
2021-03-08 00:17:58 +0900

    react dom event handler

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Say from "./Say";
+import EventPractice from "./EventPractice";
+// import Say from "./Say";
 // import Counter from "./Counter";
 // import MyComponent from "./MyComponent";
 
 
 const App = () => {
-  return <Say />
+  return <EventPractice />
 };
 
 export default App;
