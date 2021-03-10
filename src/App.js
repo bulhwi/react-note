ce0e4ff46e1c9635470c325fa9b08db14bfa7773
bulhwi <mm225012@gmail.com>
2021-03-11 01:39:36 +0900

    클래스형 컴포넌트, 함수형 컴포넌트 활용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import EventPractice from "./EventPractice";
+import FunctionalEventPractice from "./FunctionalEventPractice";
 // import Say from "./Say";
 // import Counter from "./Counter";
 // import MyComponent from "./MyComponent";
 
 
 const App = () => {
-  return <EventPractice />
+  // return <EventPractice />
+  return <FunctionalEventPractice />
 };
 
 export default App;
