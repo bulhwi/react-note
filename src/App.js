c885610ab1f70482fa9e6d72abf8afdebd23cfca
bulhwi <mm225012@gmail.com>
2021-03-12 00:59:56 +0900

    ref를 이용한 dom접근, event 호출

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import EventPractice from "./EventPractice";
-import FunctionalEventPractice from "./FunctionalEventPractice";
+import React, {Component} from 'react';
 // import Say from "./Say";
 // import Counter from "./Counter";
 // import MyComponent from "./MyComponent";
+// import EventPractice from "./EventPractice";
+// import FunctionalEventPractice from "./FunctionalEventPractice";
+import ValidationSample from './ValidationSample'
+import ScrollBox from './ScrollBox'
 
+// const App = () => {
+//   // return <EventPractice />
+//   return <ValidationSample />
+// };
 
-const App = () => {
-  // return <EventPractice />
-  return <FunctionalEventPractice />
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => {this.scrollBox=ref}}/>
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    )
+  }
+}
 
 export default App;
