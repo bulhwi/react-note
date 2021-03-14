11c72931395223f79031f5c242f1f177e6879b3a
bulhwi <mm225012@gmail.com>
2021-03-14 23:47:00 +0900

    데이터배열을 컴포넌트 배열로 반환 v-for 느낌 ;;

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import React, {Component} from 'react';
 // import MyComponent from "./MyComponent";
 // import EventPractice from "./EventPractice";
 // import FunctionalEventPractice from "./FunctionalEventPractice";
-import ValidationSample from './ValidationSample'
-import ScrollBox from './ScrollBox'
+// import ValidationSample from './ValidationSample'
+// import ScrollBox from './ScrollBox'
+import IterationSample from "./IterationSample";
 
 // const App = () => {
 //   // return <EventPractice />
@@ -15,12 +16,7 @@ import ScrollBox from './ScrollBox'
 class App extends Component {
   render() {
     return (
-      <div>
-        <ScrollBox ref={(ref) => {this.scrollBox=ref}}/>
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-      </div>
+      <IterationSample />
     )
   }
 }
