14f82503520351e1d27a801de4e03b47a33038a2
bulhwi <mm225012@gmail.com>
2021-08-04 00:02:45 +0900

    react hooks

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import Counter from './Counter';
 import Info from './Info';
+import Average from "./Average"
 
 function App() {
   const [visible, setVisible] = useState(false);
@@ -9,10 +10,13 @@ function App() {
   return (
     <div>
       <Counter />
+      <hr/>
       <button onClick={() => {setVisible(!visible)}}>
         {visible ? '숨기기': '보이기'}
       </button>
       {visible && <Info />}
+      <hr/>
+      <Average />
     </div>
   );
 }
