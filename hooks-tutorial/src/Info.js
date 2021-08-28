a35c58f78842df0d9b90f5956d3ca32eadb97e6f
bulhwi <mm225012@gmail.com>
2021-08-28 14:14:30 +0900

    컴포넌트 스타일링

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,4 +1,6 @@
 import React, {useState, useReducer} from 'react';
+import useInputs from './UseInputs';
+
 
 function reducer(state, action) {
   return {
@@ -8,70 +10,25 @@ function reducer(state, action) {
 }
 
 const Info = () => {
-  // const [name, setName] = useState('');
-  // const [nicName, setNicname] = useState('');
-
-  // useEffect(() => {
-  //   console.log("렌더링 완료되었습니다.");
-  //   console.log({
-  //     name,
-  //     nicName
-  //   })
-  // });
-  //
-  // useEffect(() => {
-  //   console.log("마운트 될때만 실행");
-  // }, []);
-  //
-  // useEffect(() => {
-  //   console.log("name : ", name);
-  // }, [name]);
-  //
-  // useEffect(() => {
-  //   console.log("name : ", name);
-  //   return () => {
-  //     console.log("cleanup")
-  //     console.log(name + "_cleanup");
-  //   }
-  // }, [name]);
-  //
-  // useEffect(() => {
-  //   console.log("effect");
-  //   return () => {
-  //     console.log("unmount");
-  //   }
-  // }, []);
-
-  // const onChangeName = e => {
-  //   setName(e.target.value);
-  // };
-  //
-  // const onChangeNicname = e => {
-  //   setNicname(e.target.value);
-  // }
-
-  const [state, dispatch] = useReducer(reducer, {
-    name:"",
-    nicName:""
+  const [state, onChange] = useInputs({
+    name: '',
+    nickname:'',
   });
 
-  const {name, nicName} = state;
-  const onChange = e => {
-    dispatch(e.target);
-  }
+  const {name, nickname} = state;
 
   return (
     <div>
       <div>
         <input name="name" value={name} onChange={onChange} />
-        <input name="nicName" value={nicName} onChange={onChange} />
+        <input name="nicName" value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
           <b>이름:</b> {name}
         </div>
         <div>
-          <b>닉네임:</b> {nicName}
+          <b>닉네임:</b> {nickname}
         </div>
       </div>
     </div>
---
+++ styling-react/README.md
@@ -0,0 +1,5 @@
+# 컴포넌트 스타일링
+
+### 일반 CSS
+* [BEM Naming](http://getbem.com/naming/)
+* CSS Selector - css 클래스가 특정 클래스 내부에 있는 경우에 스타일을 적용.
