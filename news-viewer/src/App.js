63751835f0a858e44444875cf725484758c21929
bulhwi <mm225012@gmail.com>
2021-09-23 01:23:08 +0900

    create NewsList.js, NewsItem.js

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,25 +1,8 @@
-import React, {useState} from "react";
-import axios from 'axios';
+import React from "react";
+import NewsList from "./components/NewsList";
 
 function App() {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get("https://newsapi.org/v2/top-headlines?country=kr&apiKey=0f2f5583972e4103aea1a64865a395cc");
-      setData(response.data);
-    }catch(e) {
-      console.log(e);
-    }
-  };
-
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
-    </div>
-  )
+  return <NewsList />
 }
 
 export default App;
