907f5f531b0863d17ad792695ffcaa156f8cdd35
bulhwi <mm225012@gmail.com>
2021-11-26 00:51:54 +0900

    todo component 기능 추가

--- react-redux-tutorial/src/App.js
+++ react-redux-tutorial/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import Todos from "./components/Todos.js";
 import CounterContainer from './containers/CounterContainer';
+import TodosContainer from './containers/TodoContainer';
 
 function App() {
   return (
     <div>
       <CounterContainer />
       <hr/>
-      <Todos />
+      <TodosContainer />
     </div>
   );
 }
