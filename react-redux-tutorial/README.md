5cd47a71b6bec08f8c0d2f638a2dcaa81ef5ce9d
bulhwi <mm225012@gmail.com>
2021-11-29 00:38:22 +0900

    immer를 활용한 리덕스 상태관리

--- react-redux-tutorial/README.md
+++ react-redux-tutorial/README.md
@@ -27,3 +27,10 @@ import {composeWithDevTools} from 'redux-devtools-extension';
 const store = createStore(rootReducer, composeWithDevTools());
 ...
 ```
+
+
+[immer](https://github.com/immerjs/immer#readme)   
+리듀서에서 상태를 업데이트할 때는 불변성을 지켜야 하기 때문에 spread 연산자(...)와 배열의 내장함수들(map, concat, filter 등등)을 사용하게 되는데,
+모듈의 상태가 복잡해질수록 불변성을 지키기가 까다로워진다.
+객체의 깊이가 깊지 않을수록 불변성을 지켜가면서 값을 업데이트하기가 수월하다.
+객체의 구조가 복잡해지거나, 객체로 이루어진 배열을 다룰경우 'immer'를 사용하면 편리하게 상태관리가 가능해진다.
--- react-redux-tutorial/src/modules/todos.js
+++ react-redux-tutorial/src/modules/todos.js
@@ -1,22 +1,23 @@
-import { createActions, handleActions } from "redux-actions";
+import { createAction, handleActions } from "redux-actions";
+import produce from "immer";
 
 const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋 값을 변경함.
 const INSERT = 'todos/INSERT'; // 새로운 todo를 등록함.
 const TOGGLE = 'todos/TOGGLE'; // todo를 체크.체크해제함.
 const REMOVE = 'todos/REMOVE'; // todo를 제거함.
 
-export const changeInput = createActions(CHANGE_INPUT, input => input);
+export const changeInput = createAction(CHANGE_INPUT, input => input);
 
 let id = 3; // insert가 호출될 때마다 1씩 더해집니다.
-export const insert = createActions(INSERT, text => ({
+export const insert = createAction(INSERT, text => ({
   id: id++,
   text,
   done: false,
 }));
 
-export const toggle = createActions(TOGGLE, id => id);
+export const toggle = createAction(TOGGLE, id => id);
 
-export const remove = createActions(REMOVE, id => id);
+export const remove = createAction(REMOVE, id => id);
 
 const initialState = {
   input: '',
@@ -36,22 +37,26 @@ const initialState = {
 
 const todos = handleActions(
   {
-    [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input }),
-    [INSERT]: (state, { payload: todo }) => ({
-      ...state,
-      todos: state.todos.concat(todo),
-    }),
-    [TOGGLE]: (state, { payload: id }) => ({
-      ...state,
-      todos: state.todos.map(todo =>
-        todo.id === id ? { ...todo, done: !todo.done } : todo,
-      ),
-    }),
-    [REMOVE]: (state, { payload: id }) => ({
-      todos: state.todos.filter(todo => todo.id !== id),
-    }),
-    initialState
-  }
+    [CHANGE_INPUT]: (state, { payload: input }) =>
+      produce(state, draft => {
+        draft.input = input;
+      }),
+    [INSERT]: (state, { payload: todo }) =>
+      produce(state, draft => {
+        draft.todos.push(todo);
+      }),
+    [TOGGLE]: (state, { payload: id }) =>
+      produce(state, draft => {
+        const todo = draft.todos.find(todo => todo.id === id);
+        todo.done = !todo.done;
+      }),
+    [REMOVE]: (state, { payload: id }) =>
+      produce(state, draft => {
+        const index = draft.todos.findIndex(todo => todo.id === id);
+        draft.todos.splice(index, 1);
+      })
+  },
+  initialState
 );
 
 export default todos;
