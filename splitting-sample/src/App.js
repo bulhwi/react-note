c15c3d3b87cfd59571578d3a9ff0ec1e2f561520
bulhwi <mm225012@gmail.com>
2021-12-08 01:45:53 +0900

    React.lazy, Suspense를 이용한 코드 스플리팅

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,47 +1,25 @@
-import React, { Component } from "react";
+import React, { useState, Suspense } from "react";
 import logo from './logo.svg';
 import './App.css';
-// import notify from "./notify";
+const SplitMe = React.lazy(() => import('./SplitMe'));
 
-class App extends Component {
-  state = {
-    SplitMe: null
+function App() {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
   };
 
-  handleClick = async () => {
-    const loadedModule = await import('./SplitMe');
-    this.setState({
-      SplitMe: loadedModule.default
-    });
-  };
-
-  render() {
-    const { SplitMe } = this.state;
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handleClick}>Hello React!</p>
-          { SplitMe && <SplitMe /> }
-        </header>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt='logo' />
+        <p onClick={onClick}>Hello React!</p>
+        <Suspense fallback={<div>Loading....</div>}>
+          {visible && <SplitMe />}
+        </Suspense>
+      </header>
+    </div>
+  )
 }
-
-// function App() {
-//   const onClick = () => {
-//     import("./notify").then(result => result.default());
-//   }
-//
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p onClick={onClick}>Hello React!</p>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
