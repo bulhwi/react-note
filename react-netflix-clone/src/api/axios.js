b4f162ca369be153f6dc502ec96122a7dc489a6f
bulhwi <mm225012@gmail.com>
2022-08-17 23:28:23 +0900

    netflix clone site

---
+++ react-netflix-clone/src/api/axios.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: "https://api.themviedb.org/3",
+  params: {
+    api_key: "873f5478e90211583af385d0ffbd5b1c",
+    language: "ko_KR",
+  },
+});
+
+export default instance;
