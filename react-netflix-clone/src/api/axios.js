6f4fe97218f40efe462f431403a3168595760e22
bulhwi <mm225012@gmail.com>
2022-08-18 00:16:38 +0900

    create header banner

--- react-netflix-clone/src/api/axios.js
+++ react-netflix-clone/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: "https://api.themviedb.org/3",
+  baseURL: "https://api.themoviedb.org/3",
   params: {
     api_key: "873f5478e90211583af385d0ffbd5b1c",
     language: "ko_KR",
---
+++ react-netflix-clone/src/components/Banner.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import requests from '../api/request';
+import axios from '../api/axios';
+
+const Banner = () => {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const request = await axios.get(requests.fetchNowPlaying);
+
+    const movieId =
+      request.data.results[Math.floor(Math.random() * request.data.results.length)].id;
+
+    const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
+      params: { append_to_response: 'videos' }
+    });
+    console.log(movieDetail);
+    setMovie(movieDetail);
+  };
+
+  return (
+    <header className='banner' style={{
+      backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+      backgroundPosition: 'top center',
+      backgroundSize: 'cover'
+    }}>
+      <div className='banner_contents'>
+        <h1 className='banner__title'>
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className='banner__buttons'>
+          <button className='banner__button play'>
+            Play
+          </button>
+          <button className='banner__button info'>
+            <div className='space'></div>
+            More Information
+          </button>
+        </div>
+
+        <h1 className='banner__description'>
+          {movie?.overview}
+        </h1>
+      </div>
+      <div className='banner--fadeBottom' />
+    </header>
+  );
+};
+
+export default Banner;
