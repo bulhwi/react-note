8e6c6098558fd491ba4dd2b85f9ed6292b7453a4
bulhwi <mm225012@gmail.com>
2022-08-24 21:12:27 +0900

    Row, footer, modal

--- react-netflix-clone/src/App.js
+++ react-netflix-clone/src/App.js
@@ -1,13 +1,47 @@
 import './App.css';
 import Nav from './components/Nav';
 import Banner from './components/Banner';
+import Row from './components/Row';
+import requests from './api/request';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Nav />
       <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
 
+      <Row
+        title="Trending Now"
+        id="TN"
+        fetchUrl={requests.fetchTrending}
+      />
+
+      <Row
+        title="Top Rated"
+        id="TR"
+        fetchUrl={requests.fetchTopRated}
+      />
+
+      <Row
+        title="ActionMovies"
+        id="AM"
+        fetchUrl={requests.fetchActionMovies}
+      />
+
+      <Row
+        title="Comedy Movies"
+        id="Cm"
+        fetchUrl={requests.fetchComedyMovies}
+      />
+
+      <Footer />
     </div>
   );
 }
