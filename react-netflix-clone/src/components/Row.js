832e870a87a446ef066b729af91b1e841e5bf132
bulhwi <mm225012@gmail.com>
2022-08-28 14:11:50 +0900

    swiper

--- react-netflix-clone/src/components/Row.js
+++ react-netflix-clone/src/components/Row.js
@@ -3,6 +3,14 @@ import axios from '../api/axios';
 import "./Row.css";
 import MovieModal from './MovieModal';
 
+import { Navigation, Pagination, Scrollbar, A11y} from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 const Row = ({ title, id, fetchUrl, isLargeRow }) => {
   const [movies, setMovies] = useState([]);
   const [modalOpen, setModalOpen] = useState(false);
@@ -26,32 +34,42 @@ const Row = ({ title, id, fetchUrl, isLargeRow }) => {
     <section className="row">
       <h2>{title}</h2>
       <div className="slider">
-        <div className="slider__arrow-left" onClick={() => {
-          document.getElementById(id).scrollLeft -= window.innerWidth - 80;
-        }}>
-          <span className="arrow">
-            {"<"}
-          </span>
-        </div>
-        <div id={id} className="row__posters">
-          {movies.map((movie) => (
-            <img
-              key={movie.id}
-              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-              src={`https://image.tmdb.org/t/p/original${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
-              loading="lazy"
-              alt={movie.name}
-              onClick={() => handleClick(movie)}
-            />
-          ))}
-        </div>
-        <div className="slider__arrow-right" onClick={(e) => {
-          document.getElementById(id).scrollLeft += window.innerWidth - 80;
-        }}>
-          <span className="arrow">
-            {">"}
-          </span>
-        </div>
+        {/*<div className="slider__arrow-left" onClick={() => {*/}
+        {/*  document.getElementById(id).scrollLeft -= window.innerWidth - 80;*/}
+        {/*}}>*/}
+        {/*  <span className="arrow">*/}
+        {/*    {"<"}*/}
+        {/*  </span>*/}
+        {/*</div>*/}
+        <Swiper
+          // install Swiper modules
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          navigation
+          loop={true}
+          pagination={{ clickable: true }}
+        >
+          <div id={id} className="row__posters">
+            {movies.map((movie) => (
+              <SwiperSlide>
+                <img
+                  key={movie.id}
+                  className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                  src={`https://image.tmdb.org/t/p/original${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                  loading="lazy"
+                  alt={movie.name}
+                  onClick={() => handleClick(movie)}
+                />
+              </SwiperSlide>
+            ))}
+          </div>
+        </Swiper>
+        {/*<div className="slider__arrow-right" onClick={(e) => {*/}
+        {/*  document.getElementById(id).scrollLeft += window.innerWidth - 80;*/}
+        {/*}}>*/}
+        {/*  <span className="arrow">*/}
+        {/*    {">"}*/}
+        {/*  </span>*/}
+        {/*</div>*/}
       </div>
 
       {modalOpen && (
